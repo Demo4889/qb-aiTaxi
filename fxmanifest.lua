36cde16d674ebaa3935b1553af6526957eb85521
Demo <51279632+Demo4889@users.noreply.github.com>
2022-08-07 15:46:00 -0400

    Updated fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB aiTaxi'
+
+version '1.0.0'
+
+shared_scripts {
+	'config.lua'
+}
+
+server_scripts {
+	'server/main.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
