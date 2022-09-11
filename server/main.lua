de789f2367cf0b8194a4f0473f3f140f80311eb2
Demo <51279632+Demo4889@users.noreply.github.com>
2022-09-11 11:00:10 -0400

    Update main.lua
    More QB Updates

--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,6 @@
-ESX = nil
+local QBCore = exports['qb-core']:GetCoreObject()
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-
-RegisterServerEvent('esx_aiTaxi:pay')
-AddEventHandler('esx_aiTaxi:pay', function(price)
+RegisterNetEvent('esx_aiTaxi:pay', function(price)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	xPlayer.removeMoney(price)
